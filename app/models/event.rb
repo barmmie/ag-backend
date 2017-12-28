0acdf1b90c9be0b9b7334a98656f6901ded0bb5c
Ope Aina <barmmie@gmail.com>
2017-12-28 18:45:07 +0000

    firs-pass

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,11 @@
 class Event < ApplicationRecord
   include Store::Attributes
 
-  has_many :tickets, dependent: :destroy
+  has_many :tickets, dependent: :destroy do
+    def lowest_total_price_for quantity
+      Ticket.get_cheapest(records, quantity)
+    end
+  end  
 
   validates :title, :artist, presence: true
 
@@ -13,4 +17,16 @@ class Event < ApplicationRecord
   delegate :send_order_email, :send_order_email=,
            :show_tickets_left, :show_tickets_left=,
            to: :overrides
+           
+scope :email_enabled,   -> { where("overrides ->> 'send_order_email' = 'true' OR overrides ->> 'send_order_email' is NULL") }
+scope :email_disabled,   ->{ where("overrides ->> 'send_order_email' = 'false'") }
+
+ scope :tickets_left_enabled,   -> { where("overrides ->> 'show_tickets_left' = 'true' OR overrides ->> 'send_order_email' is NULL") }
+ scope :tickets_left_disabled,   ->{ where("overrides ->> 'show_tickets_left' = 'false'") }
+ 
+ scope :tickets_more_than,   ->(length){ joins(:tickets).group("events.id").having("count(tickets.id)> ?", length) }
+ scope :ticket_count_exactly,   ->(length){ joins(:tickets).group("events.id").having("count(tickets.id) = ?", length) }
+ 
+ scope :ticket_prices_less_than,   ->(price){ joins(:tickets).where("tickets.price < ?", price).distinct }
+ 
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,4 +3,55 @@ class Ticket < ApplicationRecord
   serialize :splits, Array
 
   validates :quantity, :price, :source, :event, presence: true
+  
+  def satisfies_split_vector(quantity)
+    splits.any? { |split| quantity % split == 0 }
+  end
+  
+  def max_split_buyable(amount)
+    splits = []
+    remaining_amount = amount
+    while remaining_amount != 0
+      split = get_highest_split(remaining_amount)
+      splits << split
+      remaining_amount = remaining_amount - split
+    end
+   ( splits.count > 0) ? splits.max : 0 
+  end
+  
+  def get_highest_split(amount)
+    
+    available_splits = splits.sort.reverse
+    index = 0
+    split = available_splits[index]
+    until amount >= split
+       index = index + 1
+       split = available_splits[index]
+    end
+    
+    split
+  end
+  
+  def self.get_cheapest (tickets, quantity)
+    cheapest = nil
+    tickets.each do |ticket|
+      puts quantity
+      max_buyable = ticket.max_split_buyable(quantity)
+      
+      quantity_bought = quantity - max_buyable
+      
+      cost = ticket.price * quantity_bought
+      
+      quantity_left = quantity - quantity_bought
+      
+      if quantity_left > 0
+        cost = cost + get_cheapest(tickets, quantity_left)
+      end
+      if ( cheapest.nil? or cheapest > cost)
+        cheapest = cost
+      end
+    end
+    
+    cheapest
+  end
 end
